a06a57931c542ab3658f587ea914e33aa5ce34be
Pietro Ghezzi <pietro@pop-os.localdomain>
2021-04-12 10:08:53 +0200

    mysql kafka connector + docker-compose cleanup

---
+++ README.md
@@ -0,0 +1,15 @@
+# PoC Kafka for ADT Patient
+
+This PoC aims to evaluate the usage of Kafka for event sourcing in the development of an Admission Dismission Transfer Patient solution.
+
+## Run Kafka locally with docker
+
+- `cd docker-kafka`
+- `docker-compose up -d`
+
+*Remove all exited containers* `docker rm $(docker ps -a -f status=exited -q)`
+
+### Run ksqldb-cli
+- `docker exec -it ksqldb-cli ksql http://ksqldb-server:8088`
+- `show topics`
+- `print 'topic-name' FROM BEGINNING`
--- patient-streams/create-connectors-avroconverter.sh
+++ patient-streams/create-connectors-avroconverter.sh
@@ -8,4 +8,18 @@ echo
 curl -i -X POST http://localhost:8083/connectors \
   -H 'Content-Type: application/json' \
   -H 'Accept: application/json' \
-  -d /connectors/mysql-source-customers.json
+  -d '{
+  "name": "mysql-source-patients",
+  "config": {
+    "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+    "connection.url": "jdbc:mysql://mysql:3306/admindb?characterEncoding=UTF-8&serverTimezone=UTC",
+    "connection.user": "root",
+    "connection.password": "rootpw",
+    "table.whitelist": "patients",
+    "mode": "timestamp+incrementing",
+    "timestamp.column.name": "updated_at",
+    "incrementing.column.name": "id",
+    "topic.prefix": "mysql.admindb.",
+    "tasks.max": "1"
+  }
+}'
