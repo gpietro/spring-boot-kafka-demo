0b4cb11bd1bee876250b66a8daf3f00e3c3bd904
Ghezzi Pietro <ext01622@EOC.NET>
2021-05-14 15:30:40 +0200

    raw UI to visualize messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,41 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import {format} from 'date-fns';
 import './App.css';
 
 const App = () => {
 
+    const [patients, setPatients] = useState(new Map());
+
     useEffect(() => {
         console.log('init')
         const eventSource = new EventSource('http://localhost:9999/iq/v1/locations');
-        eventSource.onopen = (event) => console.log('open', event);
+        eventSource.onopen = (event) => console.log('sse connection opened', event);
         eventSource.onmessage = (event) => {
-            console.log('event', event.data)
-            //const location = JSON.parse(event.data).source;
-            //console.log(`location: ${location}`);
+            console.log('data', event.data)
+            const patient = JSON.parse(event.data);
+            console.log('patient', patient, '--->', patients)
+            const key = `${patient.wardId}-${patient.patientId}`;
+            setPatients(new Map(patients.set(key, patient)));
         };
-        eventSource.onerror = (event) => console.log('error', event);
+        eventSource.onerror = (event) => console.log('sse connection error', event);
     }, [])
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <h1>Patients</h1>
+            <ul>
+                {Array.from(patients.keys()).map(key => <li
+                    key={key}
+                    style={{color: patients.get(key).status === 'ACTIVE' ? 'green' : 'red'}}>
+                    [WardId {patients.get(key).wardId} {patients.get(key).roomId ? `/ Room ${patients.get(key).roomId}` : ''}
+                    {patients.get(key).bedId ? `/ Bed ${patients.get(key).bedId}` : ''}]
+                    &nbsp;#{patients.get(key).patientId} {patients.get(key).firstName} {patients.get(key).lastName} - {format(new Date(patients.get(key).birthDate), 'dd.MM.yyyy')}</li>
+                )
+                }
+            </ul>
         </div>
     );
 }
+
 
 export default App;
